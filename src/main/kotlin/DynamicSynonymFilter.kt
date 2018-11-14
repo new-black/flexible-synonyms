a6075223b3dbdd8c30e9787d661055b9c762b612
kvisscher <kevin@newblack.io>
2018-11-14 18:04:47 +0100

    fix: make filter thread safe

--- src/main/kotlin/DynamicSynonymFilter.kt
+++ src/main/kotlin/DynamicSynonymFilter.kt
@@ -4,24 +4,46 @@ import org.apache.lucene.analysis.TokenFilter
 import org.apache.lucene.analysis.TokenStream
 import org.apache.lucene.analysis.synonym.SynonymGraphFilter
 import org.apache.lucene.analysis.synonym.SynonymMap
+import java.util.concurrent.locks.ReadWriteLock
+import java.util.concurrent.locks.ReentrantReadWriteLock
 
 class DynamicSynonymFilter(
         input: TokenStream,
         synonyms: SynonymMap
 ) : TokenFilter(input) {
 
+    private val lock: ReadWriteLock = ReentrantReadWriteLock()
+
     private var filter: SynonymGraphFilter = SynonymGraphFilter(input, synonyms, true)
 
     fun update(synonyms: SynonymMap) {
-        // TODO(kevin): make atomic
+        lock.writeLock().lock()
         filter = SynonymGraphFilter(input, synonyms, true)
+        lock.writeLock().unlock()
     }
 
-    override fun incrementToken(): Boolean = filter.incrementToken()
+    override fun incrementToken(): Boolean {
+        lock.readLock().lock()
+        val success = filter.incrementToken()
+        lock.readLock().unlock()
+        return success
+    }
 
-    override fun reset() = filter.reset()
+    override fun reset() {
+        lock.readLock().lock()
+        filter.reset()
+        lock.readLock().unlock()
+    }
 
-    override fun close() = filter.close()
+    override fun close() {
+        lock.readLock().lock()
+        filter.close()
+        lock.readLock().unlock()
+    }
 
-    override fun end() = filter.end()
+    override fun end() {
+        lock.readLock().lock()
+        filter.end()
+        lock.readLock().unlock()
+    }
 }
